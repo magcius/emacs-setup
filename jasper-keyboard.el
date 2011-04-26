519ac74d2206130deb1eb7835a219bfd7e28143b
Jasper St. Pierre <jstpierre@mecheye.net>
2011-04-26 11:54:22 -0400

    Add some new keyboard and whitespace stuff.

--- jasper-keyboard.el
+++ jasper-keyboard.el
@@ -1,20 +1,39 @@
 
-(global-set-key "\C-c#"   'comment-region)
-(global-set-key "\C-c>"   'indent-region)
+(global-set-key (kbd "C-c #")   'comment-region)
+(global-set-key (kbd "C-c C-#") 'uncomment-region)
+(global-set-key (kbd "C-c >")   'indent-region)
 
 (global-set-key [C-down] 'forward-paragraph)
 (global-set-key [C-up]   'backward-paragraph)
 
 (delete-selection-mode 1)
 
-(require 'pager)
 (global-set-key [C-M-backspace] 'delete-indentation)
-(global-set-key "\C-v"   'pager-page-down)
-(global-set-key "\M-v"   'pager-page-up)
-(global-set-key [next]   'pager-page-down)
-(global-set-key [prior]  'pager-page-up)
 
 (require 'magit)
 (global-set-key "\C-xg" 'magit-status)
+
+(require 'smooth-scrolling)
+
+; C-z on Linux freezes the entire window.
+(global-unset-key "\C-z")
+(global-unset-key "\C-x\C-z")
+
+(global-set-key "\M-p" 'backward-paragraph)
+(global-set-key "\M-n" 'forward-paragraph)
+
+(defun delete-whitespace (&optional backwards)
+  "Ripped directly from delete-horizontal-space"
+  (interactive "*P")
+  (let ((orig-pos (point)))
+    (delete-region
+     orig-pos
+     (progn
+       (if backwards
+         (skip-chars-backward " \t")
+         (skip-chars-forward " \t"))
+       (constrain-to-field nil orig-pos)))))
+
+(global-set-key "\C-c\C-k" 'delete-whitespace)
 
 (provide 'jasper-keyboard)
