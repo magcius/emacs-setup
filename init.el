81261584092a0af47cd1355f7587c264e2e6e3b4
Jasper St. Pierre <jstpierre@mecheye.net>
2011-04-26 11:52:54 -0400

    Add auto-complete back.

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 (add-to-list 'load-path "~/.emacs.d/vendor/magit")
 (add-to-list 'load-path "~/.emacs.d/vendor/Pymacs")
+(add-to-list 'load-path "~/.emacs.d/vendor/auto-complete")
 
 (require 'jasper-visual)
 (require 'jasper-clipboard)
--- jasper-misc.el
+++ jasper-misc.el
@@ -1,9 +1,10 @@
 
-(require 'magit)
-
 (server-start)
 
 (require 'guess-style)
 (global-guess-style-info-mode 1)
+
+(require 'auto-complete)
+(require 'auto-complete-config)
 
 (provide 'jasper-misc)
