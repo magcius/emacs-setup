efaa2df0f369ef2c6e7726ced7222a8e12419c5a
Jasper St. Pierre <jstpierre@mecheye.net>
2011-07-14 12:32:36 -0400

    Make emacs shut up about js2 code.

--- jasper-misc.el
+++ jasper-misc.el
@@ -7,6 +7,9 @@
 (require 'guess-style)
 (global-guess-style-info-mode 1)
 
+;; Make emacs shut up about JS code :)
+(put 'js2-basic-offset 'safe-local-variable 'integerp)
+
 (require 'auto-complete)
 (require 'auto-complete-config)
 
