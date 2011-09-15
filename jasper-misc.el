93beba597d7377f8661b957b6495b10dc4669967
Jasper St. Pierre <jstpierre@mecheye.net>
2011-09-15 11:17:31 -0400

    Try out auto-complete again.

--- jasper-misc.el
+++ jasper-misc.el
@@ -12,5 +12,10 @@
 
 (require 'auto-complete)
 (require 'auto-complete-config)
+(ac-config-default)
+
+(setq ac-quick-help-delay 0.1)
+(setq ac-delay 0)
+(setq ac-auto-show-menu 0)
 
 (provide 'jasper-misc)
