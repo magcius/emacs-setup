fe70b5e1a1efe9b49fcc8d4d1e76cf02a0f33fbb
Jasper St. Pierre <jstpierre@mecheye.net>
2011-09-16 02:52:59 -0400

    Clipboard fixes

--- jasper-clipboard.el
+++ jasper-clipboard.el
@@ -1,8 +1,5 @@
 
-(setq shift-select-mode t) ; Extend marked region with shift
 (setq mouse-drag-copy-region nil)
-(setq x-select-enable-primary nil)
-(setq x-select-enable-clipboard t) ; Use X11 CLIPBOARD by default.
 
 (setq select-active-regions t)
 (global-set-key [mouse-2] 'mouse-yank-primary)
