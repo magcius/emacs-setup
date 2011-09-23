657215dfa8c22b52385d9c38bd3db615675466e0
Jasper St. Pierre <jstpierre@mecheye.net>
2011-09-22 22:43:58 -0400

    Add vala and gist.

--- jasper-misc.el
+++ jasper-misc.el
@@ -18,4 +18,12 @@
 (setq ac-delay 0)
 (setq ac-auto-show-menu 0)
 
+(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
+(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
+(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
+(add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
+(add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))
+
+(require 'gist)
+
 (provide 'jasper-misc)
