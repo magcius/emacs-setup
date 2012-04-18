632d170f1bf42efc4e263263d006205ef138438f
Jasper St. Pierre <jstpierre@mecheye.net>
2012-04-18 12:17:28 -0400

    use marmalade for vala-mode

--- jasper-misc.el
+++ jasper-misc.el
@@ -2,18 +2,9 @@
 ; Make diff-buffer-with-file not useless
 (setq diff-switches "-u")
 
-(require 'guess-style)
-(global-guess-style-info-mode 1)
-
 ;; Make emacs shut up about JS code :)
 (put 'js2-basic-offset 'safe-local-variable 'integerp)
 (put 'js-indent-level 'safe-local-variable 'integerp)
 (put 'py-indent-offset 'safe-local-variable 'integerp)
-
-(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
-(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
-(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
-(add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
-(add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))
 
 (provide 'jasper-misc)
