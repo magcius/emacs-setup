6e2ffd4386be4b6a44f145d4ec3a936a7b187b28
Jasper St. Pierre <jstpierre@mecheye.net>
2012-04-18 11:59:26 -0400

    import popup-el

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 (add-to-list 'load-path "~/.emacs.d/vendor/magit")
 (add-to-list 'load-path "~/.emacs.d/vendor/Pymacs")
+(add-to-list 'load-path "~/.emacs.d/vendor/popup-el")
 (add-to-list 'load-path "~/.emacs.d/vendor/auto-complete")
 (add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
 (add-to-list 'load-path "~/.emacs.d/vendor/django-mode")
