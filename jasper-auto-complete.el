dfb636ab88b75dc1ad4e49b23ef18c9f0004cf00
Jasper St. Pierre <jstpierre@mecheye.net>
2013-01-16 16:06:05 -0500

    auto-complete: Adjust parameters
    Maybe things will stop being slow now.

--- jasper-auto-complete.el
+++ jasper-auto-complete.el
@@ -4,10 +4,11 @@
 (ac-config-default)
 
 (setq ac-quick-help-delay 0.1)
-(setq ac-delay 0)
+(setq ac-delay 1)
 (setq ac-auto-show-menu 0)
 (setq ac-expand-on-auto-complete nil)
 (setq ac-comphist-threshold 0.7)
+(setq ac-auto-start 3)
 
 (define-key ac-completing-map "\t" 'ac-complete)
 
