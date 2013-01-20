b577c3a9b4a5ba87cbd587323dd54c86b5270775
Jasper St. Pierre <jstpierre@mecheye.net>
2013-01-19 19:27:22 -0500

    Update auto-complete

--- jasper-auto-complete.el
+++ jasper-auto-complete.el
@@ -4,8 +4,9 @@
 (ac-config-default)
 
 (setq ac-quick-help-delay 0.1)
-(setq ac-delay 0.2)
-(setq ac-auto-show-menu 0)
+(setq ac-delay 0)
+(setq ac-auto-show-menu t)
+(setq ac-candidate-menu-min 3)
 (setq ac-expand-on-auto-complete nil)
 (setq ac-comphist-threshold 0.7)
 (setq ac-auto-start 3)
