fc2feefb2ee6829e132103a6eeed20cdd0038cc2
Jasper St. Pierre <jstpierre@mecheye.net>
2014-04-01 00:02:06 -0400

    add melpa

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
 
 (package-initialize)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (require 'jasper-visual)
 (require 'jasper-clipboard)
