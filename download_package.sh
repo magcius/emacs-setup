225e1ff8ec81eea88d2fa451403ab5ea2afafeef
Jasper St. Pierre <jstpierre@mecheye.net>
2016-04-26 17:00:03 -0700

    Remove download_package.sh

--- download_package.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir vendor/
-curl -L "http://git.savannah.gnu.org/gitweb/?p=emacs.git;a=blob_plain;hb=ba08b24186711eaeb3748f3d1f23e2c2d9ed0d09;f=lisp/emacs-lisp/package.el" > vendor/package.el
